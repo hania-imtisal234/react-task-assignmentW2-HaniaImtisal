f746db9e74e7d38708fc3c5a8d06797997ad5d06
Hania Imtisal <hania.imtisal234@gmail.com>
2024-08-04 19:37:06 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className='bg-red-500'>
+        hi
+      </h2>
+    </div>
+  );
+}
+
+export default App;
